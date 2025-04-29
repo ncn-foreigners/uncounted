0fd1ceb919797ead189303ac81b521153bff080a
BERENZ <maciej.bereswewicz@ue.poznan.pl>
2025-04-29 17:54:17 +0200

    ann estimator init idea

--- R/ann-estimator.R
+++ R/ann-estimator.R
@@ -1,2 +1,8 @@
-#' @title Estimators based on paper ...
+#' @title Graph based population size estimation
+#'
+#' @description
+#' Various estimators based on the paper Chen, B., Shrivastava, A., & Steorts, R. C. (2018). Unique entity estimation with application to the Syrian conflict. The Annals of Applied Statistics, 12(2), 1039-1067.
+#' input: pairs
+#' compents: connected components using igraph
+#' estimate popsize using the estimators from the original paper
 
