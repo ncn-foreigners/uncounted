36e56956df7676a69b76c50b32d64df809163af7
angelnuff <anicze4@st.amu.edu.pl>
2025-06-07 14:36:42 +0200

    corrected parameter dimensions

--- R/zhang_model_covariates.R
+++ R/zhang_model_covariates.R
@@ -1,9 +1,12 @@
 
 zhang_model_cov <- function(m, n, N, start = 'glm', X = NULL, Z = NULL){
-                            
-  if (is.null(X)==TRUE)  {X <- matrix(1, nrow = length(n), ncol = 1)} 
+
+  if (is.null(X)==TRUE)  {X <- matrix(1, nrow = length(n), ncol = 1)}
   if (is.null(Z)==TRUE)  {Z <- matrix(1, nrow = length(n), ncol = 1)}
-  
+
+  p1 <- ncol(X)
+  p2 <- ncol(Z)
+
   df <- data.frame(
     y = m,
     x1 = log(N),
@@ -13,53 +16,60 @@ zhang_model_cov <- function(m, n, N, start = 'glm', X = NULL, Z = NULL){
   glm_fit <- glm(y ~ x1 + x2 - 1, data = df, family = poisson(link = 'log'))
   starting <- coef(glm_fit)
   if (any(!is.finite(starting))) stop('GLM - infinite values')
-  
+
+  alpha_start <- rep(starting[1], p1)  # if X, Z are given, alpha and beta must be vectors of correct length
+  beta_start <- rep(starting[2], p2)
+  phi_start <- 0.01
+
+  start_par <- c(alpha_start, beta_start, phi_start)
+
+
   # log-likelihood, gradient and hessian
-  
-  log_lik <- function(param){
-    alpha <- param[1]
-    beta <- param[2]
-    phi <- param[3]               
+
+  log_lik <- function(param){     # we will take start_par as param in optim function
+    alpha <- param[1:p1]
+    beta <- param[(p1+1):(p1+p2)]
+    phi <- param[p1+p2+1]
     -log_lik_zhang_model_cov(alpha, beta, phi, m, n, N, X, Z)    # negative because optim() minimizes
   }
-  
+
   grad_log_lik <- function(param){
-    alpha <- param[1]
-    beta <- param[2]
-    phi <- param[3]                
+    alpha <- param[1:p1]
+    beta <- param[(p1+1):(p1+p2)]
+    phi <- param[p1+p2+1]
     -grad_log_lik_zhang_model_cov(alpha, beta, phi, m, n, N, X, Z)
-  }  
-  
+  }
+
   hess_log_lik <- function(param){
-    alpha <- param[1]
-    beta <- param[2]
-    phi <- param[3]                
+    alpha <- param[1:p1]
+    beta <- param[(p1+1):(p1+p2)]
+    phi <- param[p1+p2+1]
     -hess_log_lik_zhang_model_cov(alpha, beta, phi, m, n, N, X, Z)
   }
-  
+
+
   # optimization
-  
-  start_par <- c(starting[1], starting[2], 0.01)
-  
-  obliczenia <- optim(
-    par = start_par,    
-    fn = log_lik,     
-    gr = grad_log_lik,   
-    method = 'BFGS'
+
+  optimization <- optim(
+    par = start_par,
+    fn = log_lik,
+    gr = grad_log_lik,
+    method = 'BFGS',
+    hessian = TRUE
   )
-  
-  alpha_est <- obliczenia$par[1]
-  beta_est <- obliczenia$par[2]
-  phi_est <- obliczenia$par[3]
-  xi_est <- sum(N^alpha_est)     # target parameter estimator
-  
-  
+
+  alpha_est <- optimization$par[1:p1]
+  beta_est <- optimization$par[(p1+1):(p1+p2)]
+  phi_est <- optimization$par[p1+p2+1]
+  xi_est <- sum(N^(X %*% alpha_est))     # target parameter estimator
+
+
   return(
     list(
       coefficients = c(alpha_est, beta_est, phi_est, xi_est),
-      optim_result = obliczenia)
+      optim_result = optimization)
   )
-  
+
 }
 
 
